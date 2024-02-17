7b20d56092f0c7fe09226dd853f18077c48e6c46
im2shandyou@gmail.com <104557685+im2sh@users.noreply.github.com>
2024-02-17 21:26:34 +0900

    comment: Security Config 파일 주석 추가

--- spring-security/src/main/java/com/example/springsecurity/config/SecurityConfig.java
+++ spring-security/src/main/java/com/example/springsecurity/config/SecurityConfig.java
@@ -10,6 +10,13 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
+    /*
+    * requestMatchers("/", "/login").permitAll(): "/" 및 "/login" 요청은 모든 사용자에게 허용된다.
+      requestMatchers("/admin/**").hasRole("ADMIN"): "/admin/**" 패턴의 요청은 "ADMIN" 역할을 가진 사용자에게만 허용된다.
+      requestMatchers("/my/**").hasAnyRole("ADMIN", "USER"): "/my/**" 패턴의 요청은 "ADMIN" 또는 "USER" 역할을 가진 사용자에게만 허용된다.
+      anyRequest().authenticated(): 그 외의 모든 요청은 인증된 사용자에게만 허용된다.
+     */
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
