5fd03dc926ab2d871faa8896f19efc7c56f8b8b3
im2shandyou@gmail.com <104557685+im2sh@users.noreply.github.com>
2024-02-24 21:49:39 +0900

    feat: SecurityConfig 추가

---
+++ spring-JWT/src/main/java/com/example/springjwt/config/SecurityConfig.java
@@ -0,0 +1,53 @@
+package com.example.springjwt.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        /**
+         * 경로별 인가 작업
+         */
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/", "login").permitAll()
+                        .requestMatchers("/admin").hasRole("ADMIN")
+                        .anyRequest().authenticated());
+
+        /**
+         * 세션 관리 (Stateless)
+         */
+        http
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        //csrf disable
+        http
+                .csrf((auth) -> auth.disable());
+
+        //Form 로그인 방식 disable
+        http
+                .formLogin((auth) -> auth.disable());
+
+        //httpBasic 로그인 방식 disable
+        http
+                .httpBasic((auth) -> auth.disable());
+
+        return http.build();
+    }
+}
